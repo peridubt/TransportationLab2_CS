d687b5c170f0816f0e9645022ebe3467f813b512
Leonid <ulyanov.leo.v@gmail.com>
2024-05-21 17:50:26 +0300

    Лишнюю сущность убрал

--- Road/Road.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TransportationLab2.Road;
-
-public class Road(City.City destinationCity, int length)
-{
-    public City.City DestinationCity => destinationCity;
-    public int Length => length;
-}
